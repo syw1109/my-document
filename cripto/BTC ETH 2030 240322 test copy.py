3904551cf157814996043f18c7caf7244c0e6638
SYW <meracul@naver.com>
2024-04-15 21:57:08 +0900

    20240415

--- cripto/BTC ETH 2030 240322 test copy.py
+++ cripto/BTC ETH 2030 240322 test copy.py
@@ -5,8 +5,8 @@ import datetime
 
 #231207 ma20 *0.997 이상이면 사지는 조건 추가
 #240322 SOL추가 추가
-access = "nzLOjgY3de7Ub5LEl5xtvIpcqAVls2I2CSnQ5JlH"
-secret = "gv8DMa2PCuMvpfIEpuvo7dhnhzSU8Pah4QYWcjMO"
+access = "VjbtLbzbFoAVeILkHwCC4PFc5l8lcfQJghzBVBmD"
+secret = "gL8xagr10FdayU7dWjtI5XZ1pwratIbe9xjy9Jc9"
 
 #시작가가(어제의 종가) ma20, ma30 보다 높은지 확인하기 위함 [0]은 어제종가=오늘 시가 
 def get_open_price(ticker):
@@ -19,7 +19,15 @@ def get_open_priceE(ticker):
     """09:00 시작가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     open_priceE = df.iloc[0]['close']
-    return open_priceE    
+    return open_priceE 
+
+def get_open_priceo(ticker):
+    """09:00 시작가 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    open_priceo = df.iloc[0]['close']
+    return open_priceo 
+
+   
 
 #오늘의 최저가 low가 ma20,30보다 높은지 확인하기 위함  [1] 오늘 저가
 def get_low_price(ticker):
@@ -32,7 +40,14 @@ def get_low_priceE(ticker):
     low_priceE = df.iloc[1]['low']
     return low_priceE     
 
+
+def get_low_priceo(ticker):
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    low_priceo = df.iloc[1]['low']
+    return low_priceo 
+
    
+
 
 def get_start_time(ticker):
     """시작 시간 조회"""
@@ -53,29 +68,40 @@ def get_ma20E(ticker):
     ma20E = df['close'].rolling(20).mean().iloc[-2]
     return ma20E    
 
+# def get_ma30(ticker):
+#     """30일 이동 평균선 조회"""
+#     df = pyupbit.get_ohlcv(ticker, interval="day", count=31)
+#     ma30 = df['close'].rolling(30).mean().iloc[-2]
+#     return ma30
+
 def get_ma30(ticker):
     """30일 이동 평균선 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=31)
-    ma30 = df['close'].rolling(30).mean().iloc[-2]
+    df['hykin'] = (df['high'] + df['low']+df['open']+df['close'])/4
+    ma30 = df['hykin'].rolling(30).mean().iloc[-2]
     return ma30   
 
 def get_ma30E(ticker):
     """30일 이동 평균선 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=31)
     ma30E = df['close'].rolling(30).mean().iloc[-2]
-    return ma30E         
+    return ma30E  
 
-def get_ma200(ticker):
-    """200일 이동 평균선 조회 test 용"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=201)
-    ma200 = df['close'].rolling(20).mean().iloc[-2]
-    return ma200   
 
-def get_ma200E(ticker):
-    """200일 이동 평균선 조회 test 용"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=201)
-    ma200E = df['close'].rolling(200).mean().iloc[-2]
-    return ma200E      
+def get_ma16o(ticker):
+    """16일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=17)
+    ma16o = df['close'].rolling(16).mean().iloc[-2]
+    return ma16o   
+
+
+
+def get_ma44o(ticker):
+    """44일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=45)
+    ma44o = df['close'].rolling(44).mean().iloc[-2]
+    return ma44o        
+   
 
 def get_balance(ticker):
     """잔고 조회"""
@@ -94,7 +120,11 @@ def get_current_price(ticker):
 
 def get_current_priceE(ticker):
     """현재가 조회"""
-    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]    
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]  
+
+def get_current_priceo(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]  
 
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
@@ -113,7 +143,7 @@ while True:
         current_price = get_current_price("KRW-BTC")
         ma20 = get_ma20("KRW-BTC")
         ma30 = get_ma30("KRW-BTC")        
-        ma200 = get_ma200("KRW-BTC") 
+        
 
 
         open_priceE = get_open_priceE("KRW-ETH")
@@ -121,14 +151,25 @@ while True:
         current_priceE = get_current_priceE("KRW-ETH")
         ma20E = get_ma20E("KRW-ETH")
         ma30E = get_ma30E("KRW-ETH")        
-        ma200E = get_ma200E("KRW-ETH") 
         
-        print(ma200E)
+        
+      
+        
 
         krw = get_balance("KRW")
         btc = get_balance("BTC")
         eth = get_balance("ETH")
+        sol = get_balance("SOL")
         
+        print(btc)
+        print(eth)
+        print(sol)
+        
+        open_priceS = get_open_priceS("KRW-SOL")      # --->   'NoneType' object is not subscriptable
+        low_priceS = get_low_priceS("KRW-SOL")      # --->   'NoneType' object is not subscriptable
+        current_priceo = get_current_priceo("KRW-SOL")
+        # ma16o = get_ma16o("KRW-SOL")     # --->   'NoneType' object is not subscriptable
+        ma44o = get_ma44o("KRW-SOL")  
         
 
         if open_price < ma20*1.02 or open_price < ma30*1.02:
@@ -201,13 +242,74 @@ while True:
             else:
                 if eth > 0.01:
                     sell_result = upbit.sell_market_order("KRW-ETH", eth)        
+                    
+                    
+        # if open_priceS < ma16S*1.07 or open_priceS < ma44S*1.07:
+        #     if low_priceS > ma16S*0.98 and low_priceS > ma44S*0.98 and open_priceS > ma16S and open_priceS > ma44S:
+        #         if end_time < now :
+
+        #             if eth < 0.1 and btc <0.001:
+        #                 if sol < 0.1:
+
+        #                     buy_result = upbit.buy_market_order("KRW-SOL", krw*(0.3))
+                         
+        #             if eth > 0.1 and btc < 0.001:
+        #                 if sol < 0.1:
+ 
+        #                     buy_result = upbit.buy_market_order("KRW-SOL", krw*(0.43))
+                            
+        #             if eth < 0.1 and btc > 0.001:
+        #                 if sol < 0.1:
+ 
+        #                     buy_result = upbit.buy_market_order("KRW-SOL", krw*(0.43))                            
+                            
+        #             if eth > 0.1 and btc > 0.001:
+        #                 if sol < 0.1:
+
+        #                     buy_result = upbit.buy_market_order("KRW-SOL", krw*(0.999)) 
+
+        #     else:
+        #         if sol > 0.1:
+        #             sell_result = upbit.sell_market_order("KRW-SOL", sol)                    
+                    
+
+
+        # if open_priceS > ma16S*1.07 and open_priceS > ma44S*1.07:
+
+        #     if low_priceS > ma16S and low_priceS > ma44S and open_priceS > ma16S and open_priceS > ma44S:
+        #         if end_time < now :
+
+        #             if eth < 0.1 and btc <0.001:
+        #                 if sol < 0.1:
+
+        #                     buy_result = upbit.buy_market_order("KRW-SOL", krw*(0.3))
+                         
+        #             if eth > 0.1 and btc < 0.001:
+        #                 if sol < 0.1:
+ 
+        #                     buy_result = upbit.buy_market_order("KRW-SOL", krw*(0.43))
+                            
+        #             if eth < 0.1 and btc > 0.001:
+        #                 if sol < 0.1:
+ 
+        #                     buy_result = upbit.buy_market_order("KRW-SOL", krw*(0.43))                            
+                            
+        #             if eth > 0.1 and btc > 0.001:
+        #                 if sol < 0.1:
+
+        #                     buy_result = upbit.buy_market_order("KRW-SOL", krw*(0.999)) 
+
+        #     else:
+        #         if sol > 0.1:
+        #             sell_result = upbit.sell_market_order("KRW-SOL", sol)     
 
 
 
-        time.sleep(10)
+
+        time.sleep(1)
     except Exception as e:
         print(e)
-        time.sleep(10)
+        time.sleep(1)
 
 
 
--- cripto/BTC ETH SOL 240413.py
+++ cripto/BTC ETH SOL 240413.py
@@ -13,7 +13,8 @@ def get_open_price(ticker):
     """09:00 시작가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     open_price = df.iloc[0]['close']
-    return open_price  
+    return open_price
+
 
 def get_open_priceE(ticker):
     """09:00 시작가 조회"""
@@ -41,7 +42,7 @@ def get_low_priceE(ticker):
 def get_low_priceS(ticker):
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     low_priceS = df.iloc[1]['low']
-    return low_priceS   
+    return low_priceS  
 
    
 
@@ -57,6 +58,14 @@ def get_ma20(ticker):
     ma20 = df['close'].rolling(20).mean().iloc[-2]
     return ma20 
 
+# def get_ma20(ticker):
+#     """20일 이동 평균선 조회"""
+#     df = pyupbit.get_ohlcv(ticker, interval="day", count=21)
+#     df['hykin'] = (df['high'] + df['low']+df['open']+df['close'])/4
+#     ma20 = df['hykin'].rolling(20).mean().iloc[-2]
+#     return ma20 
+
+
 def get_ma20E(ticker):
     """20일 이동 평균선 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=21)
@@ -69,10 +78,17 @@ def get_ma16S(ticker):
     ma16S = df['close'].rolling(16).mean().iloc[-2]
     return ma16S   
 
+# def get_ma30(ticker):
+#     """30일 이동 평균선 조회"""
+#     df = pyupbit.get_ohlcv(ticker, interval="day", count=31)
+#     ma30 = df['close'].rolling(30).mean().iloc[-2]
+#     return ma30 
+
 def get_ma30(ticker):
     """30일 이동 평균선 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=31)
-    ma30 = df['close'].rolling(30).mean().iloc[-2]
+    df['hykin'] = (df['high'] + df['low']+df['open']+df['close'])/4
+    ma30 = df['hykin'].rolling(30).mean().iloc[-2]
     return ma30   
 
 def get_ma30E(ticker):
@@ -87,18 +103,7 @@ def get_ma44S(ticker):
     ma44S = df['close'].rolling(44).mean().iloc[-2]
     return ma44S         
 
-#---------------------------------------------------
-def get_ma200(ticker):
-    """200일 이동 평균선 조회 test 용"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=201)
-    ma200 = df['close'].rolling(20).mean().iloc[-2]
-    return ma200   
-
-def get_ma200E(ticker):
-    """200일 이동 평균선 조회 test 용"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=201)
-    ma200E = df['close'].rolling(200).mean().iloc[-2]
-    return ma200E      
+#---------------------------------------------------     
 
 def get_balance(ticker):
     """잔고 조회"""
@@ -140,7 +145,6 @@ while True:
         current_price = get_current_price("KRW-BTC")
         ma20 = get_ma20("KRW-BTC")
         ma30 = get_ma30("KRW-BTC")        
-        ma200 = get_ma200("KRW-BTC") 
 
 
         open_priceE = get_open_priceE("KRW-ETH")
@@ -148,13 +152,8 @@ while True:
         current_priceE = get_current_priceE("KRW-ETH")
         ma20E = get_ma20E("KRW-ETH")
         ma30E = get_ma30E("KRW-ETH")        
-        ma200E = get_ma200E("KRW-ETH") 
         
-        open_priceS = get_open_priceS("KRW-SOL")
-        low_priceS = get_low_priceS("KRW-SOL")        
-        current_priceS = get_current_priceS("KRW-SOL")
-        ma16S = get_ma16S("KRW-SOL")
-        ma44S = get_ma44S("KRW-SOL")             
+           
         
       
 
@@ -163,29 +162,38 @@ while True:
         eth = get_balance("ETH")
         sol = get_balance("SOL")
         
-        print(sol)
         print(btc)
+        print(sol)
+        print(eth)
         
-#------btc1
+        
+        open_priceS = get_open_priceS("KRW-SOL")
+        low_priceS = get_low_priceS("KRW-SOL")        
+        current_priceS = get_current_priceS("KRW-SOL")
+        ma16S = get_ma16S("KRW-SOL")
+        ma44S = get_ma44S("KRW-SOL")         
+
+        
+#btc1
         if open_price < ma20*1.02 or open_price < ma30*1.02:
             if low_price > ma20*0.98 and low_price > ma30*0.98 and open_price > ma20*0.997 and open_price > ma30:  
                 if end_time1 < now :
-                    if eth < 0.01 and sol<0.1:
+                    if eth < 0.01 and sol<0.05:
                         if btc < 0.0005:
 
                             buy_result = upbit.buy_market_order("KRW-BTC", krw*(0.4))
                          
-                    if eth > 0.01 and sol < 0.1:
+                    if eth > 0.01 and sol < 0.05:
                         if btc < 0.0005:
  
                             buy_result = upbit.buy_market_order("KRW-BTC", krw*(0.57))
                             
-                    if eth < 0.01 and sol > 0.1:
+                    if eth < 0.01 and sol > 0.05:
                         if btc < 0.0005:
  
                             buy_result = upbit.buy_market_order("KRW-BTC", krw*(0.57))                            
                             
-                    if eth > 0.01 and sol > 0.1:
+                    if eth > 0.01 and sol > 0.05:
                         if btc < 0.0005:
 
                             buy_result = upbit.buy_market_order("KRW-BTC", krw*(0.999))                            
@@ -195,26 +203,26 @@ while True:
                     sell_result = upbit.sell_market_order("KRW-BTC", btc) 
                     
                                               
-#------btc2
+# btc2
         if open_price > ma20*1.02 and open_price > ma30*1.02:
             if low_price > ma20 and low_price > ma30 and open_price > ma20*0.997 and open_price > ma30:  
                 if end_time < now :
-                    if eth < 0.01 and sol<0.1:
+                    if eth < 0.01 and sol<0.05:
                         if btc < 0.0005:
 
                             buy_result = upbit.buy_market_order("KRW-BTC", krw*(0.4))
                          
-                    if eth > 0.01 and sol < 0.1:
+                    if eth > 0.01 and sol < 0.05:
                         if btc < 0.0005:
  
                             buy_result = upbit.buy_market_order("KRW-BTC", krw*(0.57))
                             
-                    if eth < 0.01 and sol > 0.1:
+                    if eth < 0.01 and sol > 0.05:
                         if btc < 0.0005:
  
                             buy_result = upbit.buy_market_order("KRW-BTC", krw*(0.57))                            
                             
-                    if eth > 0.01 and sol > 0.1:
+                    if eth > 0.01 and sol > 0.05:
                         if btc < 0.0005:
 
                             buy_result = upbit.buy_market_order("KRW-BTC", krw*(0.999))                           
@@ -223,28 +231,28 @@ while True:
                 if btc > 0.0005:
                     sell_result = upbit.sell_market_order("KRW-BTC", btc)
 
-#------eth1
+# eth1
         if open_priceE < ma20E*1.025 or open_priceE < ma30E*1.025:
 
             if low_priceE > ma20E*0.975 and low_priceE > ma30E*0.975 and open_priceE > ma20E*0.997 and open_priceE > ma30E:
                 if end_time < now :
 
-                    if btc < 0.001 and sol<0.1:
+                    if btc < 0.001 and sol<0.05:
                         if eth < 0.001:
 
                             buy_result = upbit.buy_market_order("KRW-ETH", krw*(0.3))
                          
-                    if btc > 0.001 and sol < 0.1:
+                    if btc > 0.001 and sol < 0.05:
                         if eth < 0.001:
  
                             buy_result = upbit.buy_market_order("KRW-ETH", krw*(0.43))
                             
-                    if btc < 0.001 and sol > 0.1:
+                    if btc < 0.001 and sol > 0.05:
                         if eth < 0.001:
  
                             buy_result = upbit.buy_market_order("KRW-ETH", krw*(0.43))                            
                             
-                    if btc > 0.001 and sol > 0.1:
+                    if btc > 0.001 and sol > 0.05:
                         if eth < 0.001:
 
                             buy_result = upbit.buy_market_order("KRW-ETH", krw*(0.999)) 
@@ -252,28 +260,28 @@ while True:
                 if eth > 0.01:
                     sell_result = upbit.sell_market_order("KRW-ETH", eth)                             
 
-#--eth2
+#eth2
         if open_priceE > ma20E*1.025 and open_priceE > ma30E*1.025:
 
             if low_priceE > ma20E and low_priceE > ma30E and open_priceE > ma20E*0.997 and open_priceE > ma30E:
                 if end_time < now :
 
-                    if btc < 0.001 and sol<0.1:
+                    if btc < 0.001 and sol<0.05:
                         if eth < 0.001:
 
                             buy_result = upbit.buy_market_order("KRW-ETH", krw*(0.3))
                          
-                    if btc > 0.001 and sol < 0.1:
+                    if btc > 0.001 and sol < 0.05:
                         if eth < 0.001:
  
                             buy_result = upbit.buy_market_order("KRW-ETH", krw*(0.43))
                             
-                    if btc < 0.001 and sol > 0.1:
+                    if btc < 0.001 and sol > 0.05:
                         if eth < 0.001:
  
                             buy_result = upbit.buy_market_order("KRW-ETH", krw*(0.43))                            
                             
-                    if btc > 0.001 and sol > 0.1:
+                    if btc > 0.001 and sol > 0.05:
                         if eth < 0.001:
 
                             buy_result = upbit.buy_market_order("KRW-ETH", krw*(0.999)) 
@@ -283,10 +291,10 @@ while True:
 
 
 
-#--sol1
+#sol1
         if open_priceS < ma16S*1.07 or open_priceS < ma44S*1.07:
-            if low_priceS < ma16S*0.98:
-            #if low_priceS > ma16S*0.98 and low_priceS > ma44S*0.98 and open_priceS > ma16S and open_priceS > ma44S:
+            # if low_priceS > ma16S*0.98 and low_priceS > ma44S*0.98 and open_priceS > ma16S and open_priceS > ma44S:
+            if low_priceS < ma16S*0.98 and low_priceS < ma44S*0.98 and open_priceS < ma16S and open_priceS < ma44S:
                 if end_time < now :
 
                     if eth < 0.1 and btc <0.001:
@@ -310,11 +318,11 @@ while True:
                             buy_result = upbit.buy_market_order("KRW-SOL", krw*(0.999)) 
 
             else:
-                if sol > 0.1:
+                if sol > 0.05:
                     sell_result = upbit.sell_market_order("KRW-SOL", sol)   
                     
 
-#--sol2
+#sol2
         if open_priceS > ma16S*1.07 and open_priceS > ma44S*1.07:
 
             if low_priceS > ma16S and low_priceS > ma44S and open_priceS > ma16S and open_priceS > ma44S:
@@ -341,14 +349,14 @@ while True:
                             buy_result = upbit.buy_market_order("KRW-SOL", krw*(0.999)) 
 
             else:
-                if sol > 0.1:
+                if sol > 0.05:
                     sell_result = upbit.sell_market_order("KRW-SOL", sol)                                           
 
 
-        time.sleep(15)
+        time.sleep(1)
     except Exception as e:
         print(e)
-        time.sleep(15)
+        time.sleep(1)
 
 
 
